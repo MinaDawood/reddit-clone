835cf6e2035a6af9b2367477e5b33b91d1b36c98
minadawood <mina.ziden@hotmail.com>
2021-01-24 08:48:29 +0200

    Register & Login page

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,6 +7,12 @@ import cookie from 'cookie';
 import User from '../entities/User';
 import auth from '../middleware/auth';
 
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -27,7 +33,9 @@ const register = async (req: Request, res: Response) => {
     const user = new User({ email, username, password });
 
     errors = await validate(user);
-    if (errors.length > 0) return res.status(400).json({ errors });
+    if (errors.length > 0) {
+      return res.status(400).json(mapErrors(errors));
+    }
 
     await user.save();
 
@@ -52,7 +60,7 @@ const login = async (req: Request, res: Response) => {
     }
 
     const user = await User.findOne({ username });
-    if (!user) return res.status(404).json({ error: 'User Not Fonud' });
+    if (!user) return res.status(404).json({ username: 'User Not Fonud' });
 
     const passwordMatches = await bcrypt.compare(password, user.password);
 
